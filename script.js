5fcd70f82db3d519d0e12f15d83d8b63f616245e
Lidiya135 <lidiyawati315@gmail.com>
2023-04-09 22:21:28 +0700

    responsive

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let pages =document.querySelectorAll(['section', 'footer']);
+// section aktif---------
+let pages =document.querySelectorAll('section');
 // console.log(pages);
 let navLinks =document.querySelectorAll('header nav a');
 // console.log(navLinks);
@@ -20,6 +21,24 @@ window.onscroll = () => {
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
             })
         }
-
     })
+
+    //nav sticky-------
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100)
+
+// delete menu when klik section----
+menu.classList.remove('bx-x');
+nav.classList.remove('active');
+}
+
+
+// menu icon responsive---------------
+let menu = document.querySelector('#menu-icon');
+let nav = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    nav.classList.toggle('active');
 }
